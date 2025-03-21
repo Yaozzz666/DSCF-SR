38287f5ecd6907d5fdae9a66ff400ec7ea3cbd07
Yaozzz666 <yao_zhang@sjtu.edu.cn>
2025-03-21 14:55:21 +0800

    ch26,26921126

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # --- Evaluation on LSDIR_DIV2K_valid datasets for One Method: ---
 CUDA_VISIBLE_DEVICES=0 python test_demo.py \
-    --data_dir [path to your data dir] \
-    --save_dir [path to your save dir] \
-    --model_id 0
+    --data_dir ../ \
+    --save_dir ../results \
+    --model_id 23
 
 
 # --- When only LSDIR_DIV2K_test datasets are included (For Organizer) ---
